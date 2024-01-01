cef5f82fb06417824fa1256e6577b0a2f0f77fd2
Phillip Bement <phillipb@pbement.com>
2023-12-31 16:26:26 -0800

    tried replacing BatchNorms in dcgan with LayerNorms. this seems to cause problems/non-convergence. also tried linearized gradient tanh on the generator output, but while training still converges, it seems to result in mode collapse

--- dcgan.py
+++ dcgan.py
@@ -75,6 +75,21 @@ def weights_init(m):
         nn.init.constant_(m.bias.data, 0)
 
 
+class F_LinTanh(torch.autograd.Function):
+  """ sigmoid function except with a constant derivative of 1 """
+  @staticmethod
+  def forward(self, inp):
+    return torch.tanh(inp)
+  @staticmethod
+  def backward(self, grad_out):
+    return grad_out
+
+class LinTanh(nn.Module):
+  def __init__(self):
+    super().__init__()
+  def forward(self, x):
+    return F_LinTanh.apply(x)
+
 # Generator Code
 
 class Generator(nn.Module):
@@ -83,19 +98,22 @@ class Generator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 8),
+            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, bias=False),
+            #nn.LayerNorm([ngf * 8, 4, 4]),
+            nn.BatchNorm2d(ngf*8),
             nn.ReLU(True),
             # state size. ``(ngf*8) x 4 x 4``
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4),
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, padding=1, bias=False),
+            #nn.LayerNorm([ngf * 4, 8, 8]),
+            nn.BatchNorm2d(ngf*4),
             nn.ReLU(True),
             # state size. ``(ngf*4) x 8 x 8``
-            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 2),
+            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, padding=1, bias=False),
+            #nn.LayerNorm([ngf * 2, 16, 16]),
+            nn.BatchNorm2d(ngf*2),
             nn.ReLU(True),
             # state size. ``(ngf*2) x 16 x 16``
-            nn.ConvTranspose2d( ngf * 2, nc, 15, 1, 1, bias=False),
+            nn.ConvTranspose2d( ngf * 2, nc, 13, 1, bias=False),
             # state size. ``(ngf) x 28 x 28``
             nn.Tanh()
         )
@@ -127,18 +145,20 @@ class Discriminator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is ``(nc) x 28 x 28``
-            nn.Conv2d(nc, ndf*2, 13, 1, 1, bias=False),
+            nn.Conv2d(nc, ndf*2, 13, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. ``(ndf*2) x 16 x 16``
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, padding=1, bias=False),
+            #nn.LayerNorm([ndf * 4, 8, 8]),
+            nn.BatchNorm2d(ndf*4),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. ``(ndf*4) x 8 x 8``
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, padding=1, bias=False),
+            #nn.LayerNorm([ndf * 8, 4, 4]),
+            nn.BatchNorm2d(ndf*8),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. ``(ndf*8) x 4 x 4``
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Conv2d(ndf * 8, 1, 4, 1, bias=False),
             nn.Sigmoid()
         )
 
